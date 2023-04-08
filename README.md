eba4ad77984e233eef5d08d388dc2ba7b8645df1
bowber <46023302+bowber@users.noreply.github.com>
2023-04-08 15:52:05 +0700

    Reinit project with CMake

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-**City Builder** is a game engine dedicated to creating **Tile Based, Idle** games written in C. This engine has been set up to be used in VSCode on Windows, and targets Desktop (Linux, Windows) or Web only.
+City Builder is a game engine dedicated to creating Tile Based, Idle games written in C.
 
 ## Used library
 1. [raylib](https://www.raylib.com/): A cross-platform game library, which includes: graphic, audio, font, math...
 2. [flecs](https://github.com/SanderMertens/flecs): Entity Component System library, makes it easier to simulate millions of entities effectively.
-
-## Setup
-1. Install [raylib](https://www.raylib.com/) for Windows (with-MinGW-compiler version) at `C:/raylib` (default).
--- main.c
+++ main.c
@@ -1,83 +1,73 @@
-/*******************************************************************************************
- *
- *   raylib [core] example - Basic 3d example
- *
- *   Welcome to raylib!
- *
- *   To compile example, just press F5.
- *   Note that compiled executable is placed in the same folder as .c file
- *
- *   You can find all basic examples on C:\raylib\raylib\examples folder or
- *   raylib official webpage: www.raylib.com
- *
- *   Enjoy using raylib. :)
- *
- *   This example has been created using raylib 1.0 (www.raylib.com)
- *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
- *
- *   Copyright (c) 2013-2022 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
-
 #include "raylib.h"
-
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
-
-//----------------------------------------------------------------------------------
-// Local Functions Declaration
-//----------------------------------------------------------------------------------
-static void UpdateDrawFrame(void); // Update and draw one frame
-
-//----------------------------------------------------------------------------------
-// Main entry point
-//----------------------------------------------------------------------------------
-int main()
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
+#include "raymath.h"
+#include "flecs.h"
+void main() {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "raylib");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
+    int display = GetCurrentMonitor();
+    SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+    ToggleFullscreen();
+    // Define the camera to look into our 3d world
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
-    //--------------------------------------------------------------------------------------
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
 
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
-#else
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    // DisableCursor();                    // Limit cursor to relative movement inside the window
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    while (!WindowShouldClose())        // Detect window close button or ESC key
     {
-        UpdateDrawFrame();
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera, CAMERA_FREE);
+
+        if (IsKeyDown(KEY_W)) camera.target = Vector3Add(camera.target, (Vector3){ 0.0f, 0.1f, 0.0f });
+        if (IsKeyDown(KEY_S)) camera.target = Vector3Add(camera.target, (Vector3){ 0.0f, -0.1f, 0.0f });
+        if (IsKeyDown(KEY_A)) camera.target = Vector3Add(camera.target, (Vector3){ -0.1f, 0.0f, 0.0f });
+        if (IsKeyDown(KEY_D)) camera.target = Vector3Add(camera.target, (Vector3){ 0.1f, 0.0f, 0.0f });
+
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+                DrawGrid(10, 1.0f);
+
+            EndMode3D();
+
+            DrawRectangle( 10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
+            DrawRectangleLines( 10, 10, 320, 133, BLUE);
+
+            DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+            DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+            DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+            DrawText("- Alt + Mouse Wheel Pressed to Rotate", 40, 80, 10, DARKGRAY);
+            DrawText("- Alt + Ctrl + Mouse Wheel Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY);
+            DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
     }
-#endif
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
-}
-
-// Update and draw game frame
-static void UpdateDrawFrame(void)
-{
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
-
-    ClearBackground(RAYWHITE);
-
-    DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-
-    DrawFPS(10, 10);
-
-    EndDrawing();
+    CloseWindow();     
 }
